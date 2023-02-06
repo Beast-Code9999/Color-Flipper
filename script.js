77d2514c0f03cd7d204f7655442a1b25ce49b17b
Beast Code <beastcode9999@gmail.com>
2023-02-06 11:42:39 +0900

    add cavas script

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const colorCanvas = document.getElementById('color-canvas');
+let colorCtx = colorCanvas.getContext('2d'); // creates a 2d context for the canvas
+
+// Create a Vertical Gradient(white to black)
+var color = 'rgba(0,0,255,1)';
+let gradientH = colorCtx.createLinearGradient(0, 0, colorCtx.canvas.width, 0);
+gradientH.addColorStop(0, '#fff');
+gradientH.addColorStop(1, color);
+colorCtx .fillStyle = gradientH;
+colorCtx .fillRect(0, 0, colorCtx.canvas.width, colorCtx.canvas.height);
+
+
+// Create a Vertical Gradient(white to black)
+ let gradientV = colorCtx.createLinearGradient(0, 0, 0, 300);
+ gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+ gradientV.addColorStop(1, '#000');
+ colorCtx .fillStyle = gradientV;
+ colorCtx .fillRect(0, 0, colorCtx .canvas.width, 
+ colorCtx .canvas.height); 
+
+console.log(colorCanvas)
 // footer script
 function addDynamicFooterDate() {
     const footer = document.querySelector('.footer__year');
