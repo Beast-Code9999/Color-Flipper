78151f49f5249a4e37b241887a001cdadcbb19cf
Beast Code <beastcode9999@gmail.com>
2023-02-06 13:17:15 +0900

    add fallback message on canvas tag

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const colorCanvas = document.getElementById('color-canvas');
 let colorCtx = colorCanvas.getContext('2d'); // creates a 2d context for the canvas
 
-// Create a Vertical Gradient(white to black)
+// Create a Horizontal Gradient(white to color)
 var color = 'rgba(0,0,255,1)';
 let gradientH = colorCtx.createLinearGradient(0, 0, colorCtx.canvas.width, 0);
 gradientH.addColorStop(0, '#fff');
 gradientH.addColorStop(1, color);
-colorCtx .fillStyle = gradientH;
-colorCtx .fillRect(0, 0, colorCtx.canvas.width, colorCtx.canvas.height);
+colorCtx.fillStyle = gradientH;
+colorCtx.fillRect(0, 0, colorCtx.canvas.width, colorCtx.canvas.height);
 
 
 // Create a Vertical Gradient(white to black)
  let gradientV = colorCtx.createLinearGradient(0, 0, 0, 300);
  gradientV.addColorStop(0, 'rgba(0,0,0,0)');
  gradientV.addColorStop(1, '#000');
- colorCtx .fillStyle = gradientV;
- colorCtx .fillRect(0, 0, colorCtx .canvas.width, 
- colorCtx .canvas.height); 
+ colorCtx.fillStyle = gradientV;
+ colorCtx.fillRect(0, 0, colorCtx .canvas.width, 
+ colorCtx.canvas.height); 
 
 console.log(colorCanvas)
 // footer script
